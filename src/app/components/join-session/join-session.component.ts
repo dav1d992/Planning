a81668faa4af5cb22df8a3c86c4d5395f3f37ce5
dav1d992 <daviden@live.dk>
2025-06-09 09:25:30 +0200

    No blankie

--- src/app/components/join-session/join-session.component.ts
+++ src/app/components/join-session/join-session.component.ts
@@ -1,30 +1,32 @@
-import { Component, inject } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
-import { ActivatedRoute, Router } from "@angular/router";
-import { SessionService } from "../../services/session.service";
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SessionService } from '../../services/session.service';
 
 @Component({
-	selector: "app-join-session",
-	standalone: true,
-	imports: [CommonModule, FormsModule],
-	templateUrl: "./join-session.component.html",
-	styleUrl: "./join-session.component.scss",
+  selector: 'app-join-session',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './join-session.component.html',
+  styleUrl: './join-session.component.scss',
 })
 export class JoinSessionComponent {
-	name = "";
-	private route = inject(ActivatedRoute);
-	private router = inject(Router);
-	private service = inject(SessionService);
+  name = '';
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private service = inject(SessionService);
 
-	join() {
-		const sessionId = this.route.snapshot.paramMap.get("sessionId");
-		if (!sessionId) {
-			console.error("Session ID is required");
-			return;
-		}
-		this.service.joinSession(sessionId, this.name).then(() => {
-			this.router.navigate(["/session", sessionId]);
-		});
-	}
+  join() {
+    if (this.name === '') return;
+
+    const sessionId = this.route.snapshot.paramMap.get('sessionId');
+    if (!sessionId) {
+      console.error('Session ID is required');
+      return;
+    }
+    this.service.joinSession(sessionId, this.name).then(() => {
+      this.router.navigate(['/session', sessionId]);
+    });
+  }
 }
